10f9d12dcf75788fcefca49981d748f3c5e10aec
Sestowner <owner@sestolab.pp.ua>
2021-01-11 16:38:06 +0200

    GoogleTranslateElement: Make element draggable

--- GoogleTranslateElement.js
+++ GoogleTranslateElement.js
@@ -16,7 +16,15 @@ if (!document.getElementById(gtel_id)) {
 
     const google_translate_element = document.createElement('div');
     google_translate_element.id = gtel_id;
-    google_translate_element.setAttribute('style', 'position: fixed; bottom: 0; right: 0; z-index: 99999');
+    google_translate_element.setAttribute('style', 'position: fixed; bottom: 0; right: 0; z-index: 99999; cursor: move; width: max-content; height: max-content; padding-left: 0.5em; background: linear-gradient(45deg,#fff,#fff 50%,#000 50%,#000); background-size: 2px 2px;');
+    google_translate_element.draggable = true;
+    google_translate_element.ondrag = google_translate_element.ondragend = e => {
+        e.preventDefault();
+        e.stopPropagation();
+        google_translate_element.style.top = `${e.y + (e.type === 'drag' ? google_translate_element.offsetTop : 0)}px`;
+        google_translate_element.style.left = `${e.x + (e.type === 'drag' ? google_translate_element.offsetLeft : 0)}px`;
+    };
+
     document.body.appendChild(google_translate_element);
 
     const script = document.createElement('script');
