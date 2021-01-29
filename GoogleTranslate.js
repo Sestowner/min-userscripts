7fb5ed2c037402948eab9b92b6585a7c9aa8b0c0
Sestowner <owner@sestolab.pp.ua>
2021-01-29 17:32:57 +0200

    Indent using tabs

--- GoogleTranslate.js
+++ GoogleTranslate.js
@@ -8,6 +8,6 @@ var selection = getSelection().toString();
 var gt = `https://translate.google.com/%s${navigator.language.match(/^(zh-)?\w+/g)}`;
 
 if (selection)
-    window.open(`${gt.replace('%s', '#auto/')}/${selection}`, '_blank');
+	window.open(`${gt.replace('%s', '#auto/')}/${selection}`, '_blank');
 else
-    window.open(`${gt.replace('%s', 'translate?sl=auto&tl=')}&u=${location.href}`, '_blank');
+	window.open(`${gt.replace('%s', 'translate?sl=auto&tl=')}&u=${location.href}`, '_blank');
